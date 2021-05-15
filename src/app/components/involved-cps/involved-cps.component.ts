87629163a85850946738b7c6d10d9b9611744cee
Maurizio Brini <mau.brini@stud.uniroma3.it>
2021-05-15 17:19:25 +0200

    segment chart funzionante

--- src/app/components/involved-cps/involved-cps.component.ts
+++ src/app/components/involved-cps/involved-cps.component.ts
@@ -32,12 +32,16 @@ export class InvolvedCpsComponent implements OnInit {
     this.dataSource.loadInvolvedCps();
   }
 
-  openDialog(): void{
+  openDialog(peerAS: number, peerIPAddress: string): void{
     this.dialog.open(ModalSegmentChartComponent, {
      width: '90%',
       height: '580px',
       maxHeight: '100vh',
       maxWidth: '100vw',
+      data: {
+        peerAS,
+        peerIPAddress
+      }
     });
   }
 
--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -6,6 +6,7 @@ import { CPData } from '../model/cpData';
 import { SequenceChartData } from '../model/sequenceChartData';
 import { ABBAData } from '../model/ABBAData';
 import { sequence } from '@angular/animations';
+import { numberFormat } from 'highcharts';
 
 @Injectable({
   providedIn: 'root'
@@ -33,4 +34,12 @@ export class ChartService {
   getABBAChartData(sequenceId: string): Observable<ABBAData[]>{
     return this.http.get<ABBAData[]>('https://bgpie.net/api/aspathloop/sequence/' + sequenceId);
   }
+
+  getSegmentChartData(peerAS: number, peerIPAddress: string): Observable<Array<Array<[number, number]>>> {
+    let params = new HttpParams();
+    params = params.append('peerAS', peerAS.toString());
+    params = params.append('peerIPAddress', peerIPAddress.toString());
+    return this.http.get<Array<Array<[number, number]>>>('https://bgpie.net/api/rrc/00/draw?', {params});
+  }
+
 }
